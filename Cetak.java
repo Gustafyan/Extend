ec750be55ab22d741c8e227828ef79491356550b
Gustafyan <mrgustafyanbobby@gmail.com>
2017-02-09 10:35:58 +0700

    Add files via upload

---
+++ Cetak.java
@@ -0,0 +1,18 @@
+public class Cetak {  
+   
+    public static void main(String[] okedroid) {  //method void main utama
+ 
+   Anjing object = new Anjing();
+   
+   
+   object.tampilSuara();
+   object.suaraAnjing();
+   
+   Kucing object2 = new Kucing();
+   
+   object2.tampilSuara();
+   object2.suaraKucing();  
+       
+   }
+}
+
