0f80eac75ad34bacc71bd676e2e9a590549386b1
JonathanHalim54 <edgex54@gmail.com>
2017-02-09 10:23:56 +0700

    commit Jo

---
+++ Anjing.java
@@ -0,0 +1,8 @@
+public class Anjing extends Binatang {
+   
+   public void suaraAnjing(){  
+  //method dari class Anjing
+   System.out.println("Suara Anjing : "+SuaraBinatang);
+       
+   }
+}
---
+++ Binatang.java
@@ -0,0 +1,11 @@
+public class Binatang {
+    String SuaraBinatang ,SuaraBinatang2;
+   
+    public void tampilSuara()
+{
+    SuaraBinatang = "Guk Guk Guk !";
+    SuaraBinatang2 = "Meow Meoew !";
+    //atribut dari class Binatang
+    System.out.println("Tampil Suara");
+   
+  }  
