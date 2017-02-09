791b1dc542a7eb56dd94eef4ef6cee279743b903
Gustafyan <mrgustafyanbobby@gmail.com>
2017-02-09 10:32:33 +0700

    Add files via upload

---
+++ Kucing.java
@@ -0,0 +1,12 @@
+public class Kucing extends Binatang
+{
+   
+   
+ public void suaraKucing(){
+     
+     //method dari Class Kucing
+     
+     System.out.println("Suara Kucing : "+SuaraBinatang2);
+ }  
+   
+}
